7890bb905ac2113be8939067ad26777fb04a3ff3
hieudtrung <nigel_xzzz@yahoo.com.vn>
2023-11-12 17:06:26 +0100

    + example gui in pyqt6-qml

---
+++ gui.py
@@ -0,0 +1,15 @@
+import sys
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.load("gui.qml")
+    
+    if not engine.rootObjects():
+        sys.exit(-1)
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
