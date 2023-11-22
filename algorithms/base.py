76b0d46ac388eb8b8ad9e9a14b4382df3aea23c5
hieudtrung <nigel_xzzz@yahoo.com.vn>
2023-11-22 12:39:01 +0100

    * restructure source base for C++ implementation

--- algorithms/base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC
-
-class PathFinder(ABC):
-    def find(self):
-        ...
-    
---
+++ algorithms/common.py
@@ -0,0 +1,8 @@
+from abc import ABC
+
+class RouteFinder(ABC):
+    def find(self):
+        ...
+    
+class Droplet(ABC):
+    ...
